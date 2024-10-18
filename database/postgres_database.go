7ef219198f7b5f59fbe379c7289dd0047d6bb2e9
Vastiansen <vastiansen@gmail.com>
2024-10-18 15:49:57 +0700

    add: postgres pool

--- database/postgres_database.go
+++ database/postgres_database.go
@@ -6,17 +6,22 @@ import (
 	"os"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type PostgresDatabase interface {
-	Open()
+	OpenSingle()
+	OpenPool()
 	GetConn() *pgx.Conn
-	Close()
+	GetPool() *pgxpool.Pool
+	CloseSingle()
+	ClosePool()
 }
 
 type PostgresDatabaseImpl struct {
 	log  helper.LoggerHelper
 	conn *pgx.Conn
+	pool *pgxpool.Pool
 }
 
 func NewPostgresDatabase() PostgresDatabase {
@@ -26,7 +31,7 @@ func NewPostgresDatabase() PostgresDatabase {
 	}
 }
 
-func (db *PostgresDatabaseImpl) Open() {
+func (db *PostgresDatabaseImpl) OpenSingle() {
 	conn, err := pgx.Connect(context.Background(), os.Getenv("POSTGRES_URL"))
 	if err != nil {
 		db.log.LogErrAndExit(2, err, "Failed opening postgres database connection")
@@ -35,12 +40,31 @@ func (db *PostgresDatabaseImpl) Open() {
 	db.conn = conn
 }
 
+func (db *PostgresDatabaseImpl) OpenPool() {
+	pool, err := pgxpool.New(context.Background(), os.Getenv("POSTGRES_URL"))
+	if err != nil {
+		db.log.LogErrAndExit(2, err, "Failed opening postgres database pool")
+	}
+
+	db.pool = pool
+}
+
 func (db *PostgresDatabaseImpl) GetConn() *pgx.Conn {
 	return db.conn
 }
 
-func (db *PostgresDatabaseImpl) Close() {
+func (db *PostgresDatabaseImpl) GetPool() *pgxpool.Pool {
+	return db.pool
+}
+
+func (db *PostgresDatabaseImpl) CloseSingle() {
 	if db.conn != nil {
 		db.conn.Close(context.Background())
+	}
+}
+
+func (db *PostgresDatabaseImpl) ClosePool() {
+	if db.pool != nil {
+		db.pool.Close()
 	}
 }
